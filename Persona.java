431c238a60dbf573c9ab88f0a7216fa77c92c92d
Jasso <Jasso@DESKTOP-97AP7FC>
2025-05-26 10:18:02 -0600

    modificacion clase persona, metodos

--- Persona.java
+++ Persona.java
@@ -1,3 +1,58 @@
 public class Persona {
+private Integer id;
+private String Nombre;
+private String Apellidos;
+private Integer Edad;
+
+public Persona() {
+}
+
+public Persona(Integer id, String nombre, String apellidos, Integer edad) {
+    this.id = id;
+    Nombre = nombre;
+    Apellidos = apellidos;
+    Edad = edad;
+}
+
+public Integer getId() {
+    return id;
+}
+
+public void setId(Integer id) {
+    this.id = id;
+}
+
+public String getNombre() {
+    return Nombre;
+}
+
+public void setNombre(String nombre) {
+    Nombre = nombre;
+}
+
+public String getApellidos() {
+    return Apellidos;
+}
+
+public void setApellidos(String apellidos) {
+    Apellidos = apellidos;
+}
+
+public Integer getEdad() {
+    return Edad;
+}
+
+public void setEdad(Integer edad) {
+    Edad = edad;
+}
+
+public void Concentrarse() {
+        System.out.println("concentrandose... ");
+    }
+
+    public void Viajar() {
+        System.out.println("viajando... ");
+    }
+
 
 }
